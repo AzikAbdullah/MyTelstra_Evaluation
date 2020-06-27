46957321ce36ee8d2ce964df86efe07f6d07dbbe
N, Azik Abdullah (Cognizant) <AzikAbdullah.N@cognizant.com>
2020-06-27 19:36:31 +0530

    Tableview added

---
+++ MyTelstra_Evaluation/MyTelstra_Evaluation/View Controller/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  MyTelstra_Evaluation
+//
+//  Created by N, Azik Abdullah (Cognizant) on 27/06/20.
+//  Copyright © 2020 N, Azik Abdullah (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let infoTableView = UITableView() // view
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        view.addSubview(infoTableView)
+        
+        setupTableView()
+    }
+
+    func setupTableView() {
+        infoTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        infoTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
+        infoTableView.leftAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        infoTableView.rightAnchor.constraint(equalTo:view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        infoTableView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        infoTableView.dataSource = self
+        infoTableView.delegate = self
+    }
+}
+
+extension ViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let font: UIFont = UIFont.boldSystemFont(ofSize: 14)
+
+        let labelHeight = heightForView(text: "", font: font, width: self.view.frame.size.width - 100)
+        let rowHeight = labelHeight + 50
+        return rowHeight > 100 ? rowHeight : 100 //
+    }
+    
+
+    func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat{
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        label.sizeToFit()
+        
+        return label.frame.height + 10
+    }
+}
---
+++ MyTelstra_Evaluation/MyTelstra_Evaluation/View/infoCell.swift
@@ -0,0 +1,112 @@
+//
+//  infoCell.swift
+//  MyTelstra_Evaluation
+//
+//  Created by N, Azik Abdullah (Cognizant) on 27/06/20.
+//  Copyright © 2020 N, Azik Abdullah (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class infoCell: UITableViewCell {
+        
+    let detailsImageView:UIImageView = {
+        let img = UIImageView()
+        img.contentMode = .scaleAspectFill // image will never be strecthed vertially or horizontally
+        img.translatesAutoresizingMaskIntoConstraints = false // enable autolayout
+        img.clipsToBounds = true
+        return img
+    }()
+    
+    let titleLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor =  .black
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.addSubview(detailsImageView)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(descriptionLabel)
+        
+        
+        //
+        let views: [String: Any] = [
+          "detailsImageView": detailsImageView,
+          "titleLabel": titleLabel,
+          "descriptionLabel": descriptionLabel]
+
+        //
+        var allConstraints: [NSLayoutConstraint] = []
+
+        //
+        let imageVerticalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "V:|-15-[detailsImageView(70)]",
+          metrics: nil,
+          views: views)
+        allConstraints += imageVerticalConstraints
+
+        //
+        let titleLabelVerticalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "V:|-10-[titleLabel(30)]",
+          metrics: nil,
+          views: views)
+        allConstraints += titleLabelVerticalConstraints
+
+        //
+        let descriptionLabelVerticalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "V:|-50-[descriptionLabel]",
+          metrics: nil,
+          views: views)
+        allConstraints += descriptionLabelVerticalConstraints
+        
+        //
+        let imageHorizontalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "H:|-10-[detailsImageView(70)]",
+          metrics: nil,
+          views: views)
+        allConstraints += imageHorizontalConstraints
+
+        //
+        let titleLabelHorizontalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "H:[detailsImageView]-10-[titleLabel(300)]",
+          metrics: nil,
+          views: views)
+        allConstraints += titleLabelHorizontalConstraints
+
+        //
+        let descriptionLabelHorizontalConstraints = NSLayoutConstraint.constraints(
+          withVisualFormat: "H:[detailsImageView]-10-[descriptionLabel]",
+          metrics: nil,
+          views: views)
+        allConstraints += descriptionLabelHorizontalConstraints
+
+        NSLayoutConstraint.activate(allConstraints)
+
+        descriptionLabel.topAnchor.constraint(equalTo:self.detailsImageView.centerYAnchor).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo:titleLabel.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo:self.contentView.trailingAnchor, constant:-10).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
+}
+
