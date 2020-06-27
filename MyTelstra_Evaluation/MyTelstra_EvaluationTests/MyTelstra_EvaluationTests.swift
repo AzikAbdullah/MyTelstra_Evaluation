ba18ca4748d82c292f61a3b11bc27d5f6d461b90
N, Azik Abdullah (Cognizant) <AzikAbdullah.N@cognizant.com>
2020-06-27 19:14:53 +0530

    Ctreated New project

---
+++ MyTelstra_Evaluation/MyTelstra_EvaluationTests/MyTelstra_EvaluationTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyTelstra_EvaluationTests.swift
+//  MyTelstra_EvaluationTests
+//
+//  Created by N, Azik Abdullah (Cognizant) on 27/06/20.
+//  Copyright © 2020 N, Azik Abdullah (Cognizant). All rights reserved.
+//
+
+import XCTest
+@testable import MyTelstra_Evaluation
+
+class MyTelstra_EvaluationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
