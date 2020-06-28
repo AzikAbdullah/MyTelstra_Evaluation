ed36e098235fe4ed69e84d3f71fbe9d378cb44c6
N, Azik Abdullah (Cognizant) <AzikAbdullah.N@cognizant.com>
2020-06-28 19:16:33 +0530

    LazyImage CocoaPods added

--- MyTelstra_Evaluation/MyTelstra_Evaluation/View/InfoCell.swift
+++ MyTelstra_Evaluation/MyTelstra_Evaluation/View/InfoCell.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
-
+import LazyImage
 class InfoCell: UITableViewCell {
+    
+    lazy var lazyImage:LazyImage = LazyImage()
     
     var factContent:FactContent? {
         didSet {
@@ -28,16 +30,17 @@ class InfoCell: UITableViewCell {
             }
             
             detailsImageView.image = UIImage(named: "placeholder")
-            if let imageURL = contentItem.imageHref {
-                detailsImageView.setImageFromURL(urlString: imageURL)
-            } else {
-                detailsImageView.image = UIImage(named: "placeholder")
-            }
+            self.lazyImage.show(imageView: self.detailsImageView, url: contentItem.imageHref ?? "", completion: {
+                (error:LazyImageError?) in
+                if error != nil {
+                    self.detailsImageView.image = UIImage(named: "placeholder")
+                }
+            })
         }
     }
         
-    let detailsImageView:UIImageView = {
-        let img = UIImageView()
+    let detailsImageView:LazyImageView = {
+        let img = LazyImageView()
         img.contentMode = .scaleAspectFill // image will never be strecthed vertially or horizontally
         img.translatesAutoresizingMaskIntoConstraints = false // enable autolayout
         img.clipsToBounds = true
@@ -136,21 +139,3 @@ class InfoCell: UITableViewCell {
     }
 }
 
-extension UIImageView {
-        
-    func setImageFromURL(urlString: String) {
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    
-}
